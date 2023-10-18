e6b09847c09e18b79121b0549d185a1b5b96a512
OlexandrJ <134488937+OlexandrJ@users.noreply.github.com>
2023-10-18 15:24:51 +0300

    2 comm

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,11 +1,63 @@
 import axios from "axios";
 
-axios.defaults.headers.common["x-api-key"] = "live_ lZ3UZrHE3TjxJHWbZOtxINsp5KOVf1 0QZwzrTYj9IuBZlsIMCRtiJvsM7lKi iQVA";
+axios.defaults.headers.common["x-api-key"] = "live_lZ3UZrHE3TjxJHWbZOtxINsp5KOVf10QZwzrTYj9IuBZlsIMCRtiJvsM7lKiiQVA";
 
 export function fetchBreeds() {
-  return axios.get("https://api.thecatapi.com/v1/breeds")
-    .then(response => response.data)
-    .catch(error => {
+  const selectElement = document.querySelector(".breed-select");
+  const loaderElement = document.querySelector(".loader");
+  const errorElement = document.querySelector(".error");
+
+  loaderElement.style.display = "block";
+
+  return axios
+    .get("https://api.thecatapi.com/v1/breeds")
+    .then((response) => {
+      loaderElement.style.display = "none";
+
+      const breeds = response.data;
+      breeds.forEach((breed) => {
+        const option = document.createElement("option");
+        option.value = breed.id;
+        option.textContent = breed.name;
+        selectElement.appendChild(option);
+      });
+
+      return breeds;
+    })
+    .catch((error) => {
+      loaderElement.style.display = "none";
+      errorElement.style.display = "block";
+      throw error;
+    });
+}
+
+export function fetchCatByBreed(breedId) {
+  const catInfoElement = document.querySelector(".cat-info");
+  const loaderElement = document.querySelector(".loader");
+  const errorElement = document.querySelector(".error");
+
+  loaderElement.style.display = "block";
+
+  return axios
+    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+    .then((response) => {
+
+      loaderElement.style.display = "none";
+      
+      const catData = response.data[0];
+      const catBreed = catData.breeds[0];
+      
+      const catInfoHTML = `
+        <img src="${catData.url}" alt="${catBreed.name}">
+        <h2>${catBreed.name}</h2>
+        <p><strong>Description:</strong> ${catBreed.description}</p>
+        <p><strong>Temperament:</strong> ${catBreed.temperament}</p>
+      `;
+      catInfoElement.innerHTML = catInfoHTML;
+    })
+    .catch((error) => {
+      loaderElement.style.display = "none";
+      errorElement.style.display = "block";
       throw error;
     });
 }
--- src/index.js
+++ src/index.js
@@ -1,53 +1,10 @@
-import { fetchBreeds } from "./cat-api";
+import { fetchBreeds, fetchCatByBreed } from "./cat-api.js";
 
-const breedSelect = document.querySelector("select.breed-select");
-const catInfo = document.querySelector("div.cat-info");
-const loader = document.querySelector("p.loader");
-const error = document.querySelector("p.error");
+const selectElement = document.querySelector(".breed-select");
 
-function displayError(message) {
-  error.textContent = message;
-  error.style.display = "block";
-  loader.style.display = "none";
-  catInfo.style.display = "none";
-}
-
-breedSelect.addEventListener("change", () => {
-  const selectedBreedId = breedSelect.value;
-
-  loader.style.display = "block";
-  error.style.display = "none";
-  catInfo.style.display = "none";
-
-  if (selectedBreedId) {
-    fetchCatByBreed(selectedBreedId)
-      .then(catData => {
-        const { name, description, temperament } = catData[0].breeds[0];
-        catInfo.innerHTML = `
-          <p>Назва породи: ${name}</p>
-          <p>Опис: ${description}</p>
-          <p>Темперамент: ${temperament}</p>
-          <img src="${catData[0].url}" alt="${name}" />
-        `;
-        loader.style.display = "none";
-        catInfo.style.display = "block";
-      })
-      .catch(error => {
-        displayError("Помилка при завантаженні інформації про кота.");
-      });
-  } else {
-    loader.style.display = "none";
-    catInfo.style.display = "none";
-  }
-});
-
-fetchBreeds()
-  .then(breeds => {
-    breedSelect.innerHTML = breeds.map(breed => `
-      <option value="${breed.id}">${breed.name}</option>
-    `).join("");
-    loader.style.display = "none";
-  })
-  .catch(error => {
-    displayError("Помилка при завантаженні списку порід.");
+fetchBreeds().then((breeds) => {
+  selectElement.addEventListener("change", (event) => {
+    const selectedBreedId = event.target.value;
+    fetchCatByBreed(selectedBreedId);
   });
+});
